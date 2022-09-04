240dcb381eff6897721a4286d2fb8bb51e1eea13
Jose Isai Reyes Hernandez <79279643+IsaiStormBlesed@users.noreply.github.com>
2022-09-04 14:15:24 -0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "rails-test.up.railway.app"
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
